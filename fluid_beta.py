4a478ca4774f83321fbe2a3c4177826a924405a4
seantyh <seantyh@gmail.com>
2018-05-02 12:14:40 +0800

    add fluid_seg interface to FluidSeg

--- fluid_beta.py
+++ fluid_beta.py
@@ -1,11 +1,24 @@
 import logging
 from flask import Blueprint, request
-from flask import jsonify
+from flask import jsonify, make_response
 from fluid_seg_test_data import get_fluid_seg_test_data
+from fluid_seg import fluid_seg
+from FluidSeg import LexiconFactory
+from os.path import join, abspath, dirname
+import pdb
 
 logger = logging.getLogger("FluidSeg.FluidBeta")
-
 fluid_beta_bp = Blueprint('fluid_beta', __name__)
+
+basepath = abspath(dirname(__file__))
+lexicon = LexiconFactory().get(join(basepath, "data/fluid_seg_lexicon.txt"))
+lexicon.addSupplementary([
+    "我想說", "今天來說", "的話", "今天來說的話", "要不要再研究看看"
+])
+def make_400_response(msg):
+    resp = jsonify({"status": "failed", "message": msg})
+    resp.status_code = 400
+    return resp
 
 def generate_tag_data(tagName):    
     tag = {"tag": tagName, "pos":[0, 2], "text": "1fd34"}
@@ -16,10 +29,20 @@ def index():
     ret = {"status": "ready", "message": "fluidBeta API ready"}
     return jsonify(ret)
 
-@fluid_beta_bp.route("/text")
+@fluid_beta_bp.route("/text", methods=["POST", "GET"])
 def input_text():    
-    text = "今天來說的話，我想說，不知道大家要不要再研究看看。"
-    fluidSeg = get_fluid_seg_test_data()
+    data = request.get_json(silent=True)
+    print(data)
+    if not data:
+        return make_400_response("Cannot get json data")
+    
+    if "text" not in data or not data["text"]:
+        print("invalid data: ", str(data))
+        return make_400_response("cannot parse data")
+
+    text = data["text"]
+    # pdb.set_trace()
+    fluidSeg = fluid_seg(text, lexicon)
     ret = {
         "status": "ready", 
         "fluidSeg": fluidSeg,
---
+++ fluid_seg.py
@@ -0,0 +1,39 @@
+from os.path import abspath, dirname
+from itertools import chain
+import pyOceanus
+from FluidSeg import FluidSeg
+from FluidSeg import TokenData
+import config
+
+oc = pyOceanus.Oceanus(config.OCEANUS_ENDPOINT)
+
+def fluid_seg(text, lexicon):
+
+    fseg = FluidSeg(lexicon)
+    segData = fseg.segment(text)
+    try:
+        od = oc.parse(text)        
+        preseg = list(chain.from_iterable(od.tokens))
+        preseg = [TokenData(x[0], x[3], x[4]) for x in preseg]
+    except Exception as ex:
+        print("cannot process text content")
+        return flask.make_response("cannot process text content", 400)
+    
+    segData.setPresegment(preseg)
+    gran_label = ["0.00", "0.33", "0.66", "1.00", "preseg", "token"]
+    seg_list = [
+            segData.toSegmentedToken(segData.preseg, granularity=0.00),
+            segData.toSegmentedToken(segData.preseg, granularity=0.33),
+            segData.toSegmentedToken(segData.preseg, granularity=0.66),
+            segData.toSegmentedToken(segData.preseg, granularity=1.00),
+            segData.toSegmentedToken(segData.preseg),
+            segData.toSegmentedToken(segData.tokens),
+            ]
+    seg_obj_list = []
+    for seg in seg_list:
+        seg_obj_list.append([tok.__dict__ for tok in seg])
+
+    return {"fluidSeg": {
+                "granularity": gran_label, 
+                "segments": seg_obj_list }, 
+            "text": text }
